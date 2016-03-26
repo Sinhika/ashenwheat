22b20238b71cbb61a9eb2ca23092f1770cec9936
Sinhika <sinhika@republicofnewhome.org>
2016-03-26 09:56:29 -0500

    started working on 1.9 version

--- src/main/java/akkamaddi/ashenwheat/block/AkkamaddiCrop.java
+++ src/main/java/akkamaddi/ashenwheat/block/AkkamaddiCrop.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.IGrowable;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.BlockPos;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
