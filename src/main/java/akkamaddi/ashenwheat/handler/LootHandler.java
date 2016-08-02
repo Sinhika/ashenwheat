5a05227142305c9098987b84932f408c54cd2205
Sinhika <sinhika@republicofnewhome.org>
2016-08-02 07:14:57 -0500

    revised loot handler range-count arguments to be obtained from a factory, not created once staticly. Hopefully clears up suspiciously same item counts in loot chests

--- src/main/java/akkamaddi/ashenwheat/handler/LootHandler.java
+++ src/main/java/akkamaddi/ashenwheat/handler/LootHandler.java
@@ -13,129 +13,121 @@ import akkamaddi.ashenwheat.Content;
 
 public class LootHandler 
 {
+	/** create LootFunction ranges */
+	protected static LootFunction [] createCount(int min, int max) 
+	{
+		LootFunction [] result = new LootFunction [1];
+		result[0] = new SetCount(new LootCondition[0], new RandomValueRange(min, max));
+		return result;
+	}
+	
 	/**
 	 * add mods seeds to loot tables. 
 	 */
 	@SubscribeEvent
 	public void LootLoad(LootTableLoadEvent event)
 	{
-		// we use some of these ranges more than once...
-		LootFunction lf1to2[] = { 
-				new SetCount(new LootCondition[0], new RandomValueRange(1, 2))};
-		LootFunction lf1to3[] = { 
-				new SetCount(new LootCondition[0], new RandomValueRange(1, 3))};
-		LootFunction lf1to4[] = { 
-				new SetCount(new LootCondition[0], new RandomValueRange(1, 4))};
-		LootFunction lf2to4[] = { 
-				new SetCount(new LootCondition[0], new RandomValueRange(2, 4)) };
-		LootFunction lf2to6[] = { 
-			new SetCount(new LootCondition[0], new RandomValueRange(2, 6)) };
-		LootFunction lf2to8[] = { 
-				new SetCount(new LootCondition[0], new RandomValueRange(2, 8)) };
-		LootFunction lf6to16[] = { 
-				new SetCount(new LootCondition[0], new RandomValueRange(6, 16)) };
-
 		// add loot to pyramids, dungeons, blacksmith, mineshafts, etc
 		if (event.getName().equals(LootTableList.CHESTS_SIMPLE_DUNGEON))
 		{
 			LootPool pool1 = event.getTable().getPool("pool1");
-			pool1.addEntry(new LootEntryItem(Content.ashSeeds, 15, 0, lf2to6,
+			pool1.addEntry(new LootEntryItem(Content.ashSeeds, 15, 0, createCount(2,6),
 					new LootCondition[0], 
 					Content.ashSeeds.getUnlocalizedName().substring(5)));
 			pool1.addEntry(new LootEntryItem(Content.scintillaSeeds, 5, 0,
-					lf1to3, new LootCondition[0], 
+					createCount(1,3), new LootCondition[0], 
 					Content.scintillaSeeds.getUnlocalizedName().substring(5)));
-			pool1.addEntry(new LootEntryItem(Content.ossidSeeds, 10, 0, lf2to4,
+			pool1.addEntry(new LootEntryItem(Content.ossidSeeds, 10, 0, createCount(2,4),
 					new LootCondition[0], 
 					Content.ossidSeeds.getUnlocalizedName().substring(5)));
 			pool1.addEntry(new LootEntryItem(Content.thunderSeeds, 10, 0,
-					lf2to4, new LootCondition[0], 
+					createCount(2,4), new LootCondition[0], 
 					Content.thunderSeeds.getUnlocalizedName().substring(5)));
 		} // end-if CHESTS_SIMPLE_DUNGEON
 		else if (event.getName().equals(LootTableList.CHESTS_JUNGLE_TEMPLE))
 		{
 			LootPool pool_main = event.getTable().getPool("main");
-			pool_main.addEntry(new LootEntryItem(Content.ashSeeds, 20, 0, lf2to8,
+			pool_main.addEntry(new LootEntryItem(Content.ashSeeds, 20, 0, createCount(2,8),
 					new LootCondition[0], 
 					Content.ashSeeds.getUnlocalizedName().substring(5)+"#1"));
 			pool_main.addEntry(new LootEntryItem(Content.scintillaSeeds, 10, 0,
-					lf2to4, new LootCondition[0], Content.scintillaSeeds
+					createCount(2,4), new LootCondition[0], Content.scintillaSeeds
 							.getUnlocalizedName().substring(5)+"#1"));
-			pool_main.addEntry(new LootEntryItem(Content.ossidSeeds, 15, 0, lf2to6,
+			pool_main.addEntry(new LootEntryItem(Content.ossidSeeds, 15, 0, createCount(2,6),
 					new LootCondition[0], Content.ossidSeeds
 							.getUnlocalizedName().substring(5)+"#1"));
 			pool_main.addEntry(new LootEntryItem(Content.thunderSeeds, 15, 0,
-					lf2to6, new LootCondition[0], Content.thunderSeeds
+					createCount(2,6), new LootCondition[0], Content.thunderSeeds
 							.getUnlocalizedName().substring(5)+"#1"));
 		} // end-else-if CHESTS_JUNGLE_TEMPLE
 		else if (event.getName().equals(LootTableList.CHESTS_DESERT_PYRAMID))
 		{
 			LootPool pool1 = event.getTable().getPool("pool1");
 			pool1.addEntry(new LootEntryItem(Content.scintillaSeeds, 6, 0,
-					lf2to6, new LootCondition[0], Content.scintillaSeeds
+					createCount(2,6), new LootCondition[0], Content.scintillaSeeds
 							.getUnlocalizedName().substring(5)+"#2"));
-			pool1.addEntry(new LootEntryItem(Content.scintillaWheatSheaf, 16, 0, lf2to4,
+			pool1.addEntry(new LootEntryItem(Content.scintillaWheatSheaf, 16, 0, createCount(2,4),
 					new LootCondition[0], Content.scintillaWheatSheaf.getUnlocalizedName()
 							.substring(5)));
-			pool1.addEntry(new LootEntryItem(Content.ashCookie, 12, 0, lf6to16,
+			pool1.addEntry(new LootEntryItem(Content.ashCookie, 12, 0, createCount(6,16),
 					new LootCondition[0], Content.ashCookie
 							.getUnlocalizedName().substring(5)));
 			pool1.addEntry(new LootEntryItem(Content.scintillaCookie, 12, 0,
-					lf6to16, new LootCondition[0], Content.scintillaCookie
+					createCount(6,16), new LootCondition[0], Content.scintillaCookie
 							.getUnlocalizedName().substring(5)));
 		} // end-else-if CHESTS_DESERT_PYRAMID
 		else if (event.getName().equals(LootTableList.CHESTS_VILLAGE_BLACKSMITH))
 		{
 			LootPool pool_main = event.getTable().getPool("main");
-			pool_main.addEntry(new LootEntryItem(Content.ashSeeds, 10, 0, lf2to6,
+			pool_main.addEntry(new LootEntryItem(Content.ashSeeds, 10, 0, createCount(2,6),
 					new LootCondition[0], Content.ashSeeds.getUnlocalizedName()
 							.substring(5)+"#2"));
 			pool_main.addEntry(new LootEntryItem(Content.scintillaSeeds, 2, 0,
-					lf1to3, new LootCondition[0], Content.scintillaSeeds
+					createCount(1,3), new LootCondition[0], Content.scintillaSeeds
 							.getUnlocalizedName().substring(5)+"#3"));
-			pool_main.addEntry(new LootEntryItem(Content.ossidSeeds, 5, 0, lf2to4,
+			pool_main.addEntry(new LootEntryItem(Content.ossidSeeds, 5, 0, createCount(2,4),
 					new LootCondition[0], Content.ossidSeeds
 							.getUnlocalizedName().substring(5)+"#2"));
 			pool_main.addEntry(new LootEntryItem(Content.thunderSeeds, 5, 0,
-					lf2to4, new LootCondition[0], Content.thunderSeeds
+					createCount(2,4), new LootCondition[0], Content.thunderSeeds
 							.getUnlocalizedName().substring(5)+"#2"));
-			pool_main.addEntry(new LootEntryItem(Content.ashWheatSheaf, 5, 0, lf2to4,
+			pool_main.addEntry(new LootEntryItem(Content.ashWheatSheaf, 5, 0, createCount(2,4),
 					new LootCondition[0], Content.ashWheatSheaf.getUnlocalizedName()
 							.substring(5)));
-			pool_main.addEntry(new LootEntryItem(Content.scintillaWheatSheaf, 2, 0, lf1to3,
+			pool_main.addEntry(new LootEntryItem(Content.scintillaWheatSheaf, 2, 0, createCount(1,3),
 					new LootCondition[0], Content.scintillaWheatSheaf.getUnlocalizedName()
 							.substring(5)+"#1"));
-			pool_main.addEntry(new LootEntryItem(Content.ashCookie, 4, 0, lf6to16,
+			pool_main.addEntry(new LootEntryItem(Content.ashCookie, 4, 0, createCount(6,16),
 					new LootCondition[0], Content.ashCookie.getUnlocalizedName()
 							.substring(5)+"#1"));
-			pool_main.addEntry(new LootEntryItem(Content.scintillaCookie, 2, 0, lf6to16,
+			pool_main.addEntry(new LootEntryItem(Content.scintillaCookie, 2, 0, createCount(6,16),
 					new LootCondition[0], Content.scintillaCookie.getUnlocalizedName()
 							.substring(5)+"#1"));
 		} // end-else-if CHESTS_VILLAGE_BLACKSMITH
 		else if (event.getName().equals(LootTableList.CHESTS_ABANDONED_MINESHAFT))
 		{
 			LootPool pool1 = event.getTable().getPool("pool1");
-			pool1.addEntry(new LootEntryItem(Content.ashSeeds, 3, 0, lf1to4,
+			pool1.addEntry(new LootEntryItem(Content.ashSeeds, 3, 0, createCount(1,4),
 					new LootCondition[0], Content.ashSeeds.getUnlocalizedName()
 							.substring(5)+"#3"));
 			pool1.addEntry(new LootEntryItem(Content.scintillaSeeds, 1, 0,
-					lf1to2, new LootCondition[0], Content.scintillaSeeds
+					createCount(1,2), new LootCondition[0], Content.scintillaSeeds
 							.getUnlocalizedName().substring(5)+"#4"));
-			pool1.addEntry(new LootEntryItem(Content.ossidSeeds, 2, 0, lf1to3,
+			pool1.addEntry(new LootEntryItem(Content.ossidSeeds, 2, 0, createCount(1,3),
 					new LootCondition[0], Content.ossidSeeds
 							.getUnlocalizedName().substring(5)+"#3"));
 			pool1.addEntry(new LootEntryItem(Content.thunderSeeds, 2, 0,
-					lf1to3, new LootCondition[0], Content.thunderSeeds
+					createCount(1,3), new LootCondition[0], Content.thunderSeeds
 							.getUnlocalizedName().substring(5)+"#3"));
 		} // end else-if CHESTS_ABANDONED_MINESHAFT
 		// add in new structure igloo
 		else if (event.getName().equals(LootTableList.CHESTS_IGLOO_CHEST))
 		{
 			LootPool pool_main = event.getTable().getPool("main");
-			pool_main.addEntry(new LootEntryItem(Content.ashWheatSheaf, 9, 0, lf2to4,
+			pool_main.addEntry(new LootEntryItem(Content.ashWheatSheaf, 9, 0, createCount(2,4),
 					new LootCondition[0], Content.ashWheatSheaf.getUnlocalizedName()
 							.substring(5)));
-			pool_main.addEntry(new LootEntryItem(Content.ashBread, 14, 0, lf1to3,
+			pool_main.addEntry(new LootEntryItem(Content.ashBread, 14, 0, createCount(1,3),
 					new LootCondition[0], Content.ashBread.getUnlocalizedName()
 							.substring(5)));
 		} // end else-if CHESTS_IGLOO_CHEST
