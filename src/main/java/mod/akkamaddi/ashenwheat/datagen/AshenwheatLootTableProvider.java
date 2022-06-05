a9a173827a7831b9846de6ef4f6809bafb33085a
Sinhika <sinhika@republicofnewhome.org>
2022-06-04 19:29:59 -0500

    tried to add crops to loot table

--- src/main/java/mod/akkamaddi/ashenwheat/datagen/AshenwheatLootTableProvider.java
+++ src/main/java/mod/akkamaddi/ashenwheat/datagen/AshenwheatLootTableProvider.java
@@ -7,13 +7,31 @@ import java.util.function.Supplier;
 
 import com.mojang.datafixers.util.Pair;
 
+import mod.akkamaddi.ashenwheat.content.ModCropsBlock;
+import mod.akkamaddi.ashenwheat.content.RottenPlantBlock;
 import mod.akkamaddi.ashenwheat.init.ModBlocks;
+import mod.akkamaddi.ashenwheat.init.ModItems;
 import mod.alexndr.simplecorelib.api.datagen.BlockLootTableProvider;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.LootTable.Builder;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 public class AshenwheatLootTableProvider extends BlockLootTableProvider
 {
@@ -28,7 +46,39 @@ public class AshenwheatLootTableProvider extends BlockLootTableProvider
     {
         tables.clear();
         specialDropTable(ModBlocks.buried_remains.get(), Items.ROTTEN_FLESH);
+        cropDropTable(ModBlocks.rotten_crop.get(), Items.ROTTEN_FLESH, 1, ModItems.rotten_seeds.get(), 1, 2);
         return tables;
     }
 
+    protected void cropDropTable(ModCropsBlock cropBlock, Item primary_drop, int max_primary, ItemNameBlockItem seed_item, 
+                                 int max_seeds, int max_age)
+    {
+        blockTable(cropBlock, 
+                LootTable.lootTable()
+                    .withPool(createCropDrops(cropBlock, primary_drop, max_primary, seed_item, max_seeds, max_age)));
+    } // end CropDropTable()
+    
+    protected static LootPool.Builder createCropDrops(Block cb, Item primary_drop, int max_primary, 
+                                                      ItemNameBlockItem seed_item, int max_seeds, int max_age)
+    {
+        IntegerProperty prop = (cb instanceof RottenPlantBlock) ? RottenPlantBlock.AGE : CropBlock.AGE;
+        
+         LootPoolEntryContainer.Builder<?> builder = LootItem.lootTableItem(primary_drop)
+                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, max_primary)))
+                 .when(ExplosionCondition.survivesExplosion())
+                 .when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(cb)
+                         .setProperties(StatePropertiesPredicate.Builder.properties()
+                                 .hasProperty(prop, max_age)))
+                 .otherwise(LootItem.lootTableItem(seed_item).apply(SetItemCountFunction.setCount(ConstantValue.exactly(1.0F))));
+         
+         LootPoolEntryContainer.Builder<?> builder2 = LootItem.lootTableItem(seed_item)
+                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, max_seeds)))
+                 .when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(cb)
+                         .setProperties(StatePropertiesPredicate.Builder.properties()
+                                 .hasProperty(prop, max_age)));
+                 
+         return LootPool.lootPool().add(builder).add(builder2);
+         
+    } // end createCropDrops()
+    
 } // end class
