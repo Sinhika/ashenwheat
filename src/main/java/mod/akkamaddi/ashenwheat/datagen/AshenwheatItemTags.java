c1103b6e9028d3b25fd4695428166a94320e0202
Sinhika <sinhika@republicofnewhome.org>
2022-06-10 20:03:38 -0500

    added datagen for blaze wood-related ITEM tags, and added generated tag jsons

--- src/main/java/mod/akkamaddi/ashenwheat/datagen/AshenwheatItemTags.java
+++ src/main/java/mod/akkamaddi/ashenwheat/datagen/AshenwheatItemTags.java
@@ -1,7 +1,9 @@
 package mod.akkamaddi.ashenwheat.datagen;
 
 import mod.akkamaddi.ashenwheat.Ashenwheat;
+import mod.akkamaddi.ashenwheat.init.ModBlocks;
 import mod.alexndr.simplecorelib.api.datagen.MiningItemTags;
+import mod.alexndr.simplecorelib.api.helpers.TagUtils;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -13,4 +15,38 @@ public class AshenwheatItemTags extends MiningItemTags
         super(gen, new AshenwheatBlockTags(gen, existingFileHelper), Ashenwheat.MODID, existingFileHelper);
     }
 
+    @Override
+    protected void addTags()
+    {
+        super.addTags();
+        registerLogTags();
+    }
+
+    protected void registerLogTags()
+    {
+        this.tag(TagUtils.modTag(Ashenwheat.MODID, "blaze_logs"))
+            .add(ModBlocks.blaze_log.get().asItem())
+            .add(ModBlocks.blaze_wood.get().asItem())
+            .add(ModBlocks.stripped_blaze_log.get().asItem())
+            .add(ModBlocks.stripped_blaze_wood.get().asItem());
+        
+        this.tag(TagUtils.modTag("minecraft", "non_flammable_wood"))
+            .add(ModBlocks.blaze_log.get().asItem())
+            .add(ModBlocks.blaze_wood.get().asItem())
+            .add(ModBlocks.stripped_blaze_log.get().asItem());
+        
+        this.tag(TagUtils.modTag("minecraft", "planks"))
+            .add(ModBlocks.blaze_planks.get().asItem());
+        
+        this.tag(TagUtils.modTag("minecraft", "leaves"))
+            .add(ModBlocks.blaze_leaves.get().asItem());
+        
+        this.tag(TagUtils.modTag("minecraft", "saplings"))
+            .add(ModBlocks.blaze_sapling.get().asItem());
+        
+        this.tag(TagUtils.modTag("minecraft", "logs"))
+            .addTag(TagUtils.modTag(Ashenwheat.MODID, "blaze_logs"));
+   
+   } // end registerLogTags()
+  
 } // end class
