950191f6165dca1e50914e92a4da0789921223e6
Sinhika <sinhika@republicofnewhome.org>
2022-09-04 11:35:00 -0500

    added more feature jsons. Removing obsolete feature classes

--- src/main/java/mod/akkamaddi/ashenwheat/init/ModFeatures.java
+++ src/main/java/mod/akkamaddi/ashenwheat/init/ModFeatures.java
@@ -22,12 +22,6 @@ public class ModFeatures
     public static final DeferredRegister<Feature<?>> MOD_FEATURES =
             DeferredRegister.create(Registry.FEATURE_REGISTRY, Ashenwheat.MODID);
             
-//    public static RegistryObject<Feature<GlowLichenConfiguration>> ROTTEN_PLANT_FEATURE =
-//            MOD_FEATURES.register("rotten_plant_feature", ()->new CavePlantFeature(GlowLichenConfiguration.CODEC, ModBlocks.rotten_crop.get()));
-//                    
-//    public static RegistryObject<Feature<CountConfiguration>> ENDER_CLAM_FEATURE =
-//            MOD_FEATURES.register("ender_clam_feature", ()->new EnderClamFeature(CountConfiguration.CODEC)); 
-//    
 //    public static RegistryObject<Feature<CountConfiguration>> SHALLOW_GRAVE_FEATURE =
 //            MOD_FEATURES.register("buried_remains_feature", ()->new ShallowGraveFeature(CountConfiguration.CODEC));
     
@@ -40,34 +34,11 @@ public class ModFeatures
     public static RegistryObject<ConfiguredFeature<TreeConfiguration, ?>> BLAZE_TREE = 
             CONFIGURED_FEATURES.register("blaze_tree", () -> new ConfiguredFeature<>(Feature.TREE, createBlazeTree().build()));
 //    
-//    // RandomPatchConfiguration - equivalent of a VegetationFeatures entry.
-//    public static RegistryObject<ConfiguredFeature<RandomPatchConfiguration,?>> PATCH_FLAX =
-//            CONFIGURED_FEATURES.register("patch_flax", 
-//                    ()-> new ConfiguredFeature<>(Feature.RANDOM_PATCH, 
-//                            FeatureUtils.simplePatchConfiguration(Feature.SIMPLE_BLOCK, 
-//                                    new SimpleBlockConfiguration(BlockStateProvider.simple(
-//                                            ModBlocks.flax_crop.get().defaultBlockState().setValue(CropBlock.AGE, 
-//                                                                                            Integer.valueOf(CropBlock.MAX_AGE)))),
-//                                    List.of(Blocks.GRASS_BLOCK, Blocks.DIRT))));
-//    
-//    public static RegistryObject<ConfiguredFeature<CountConfiguration, ?>> ENDER_CLAM =
-//            CONFIGURED_FEATURES.register("ender_clam", 
-//                    () -> new ConfiguredFeature<>(ENDER_CLAM_FEATURE.get(),
-//                            new CountConfiguration(3)));
-//    
 //    public static RegistryObject<ConfiguredFeature<CountConfiguration, ?>> ORE_BURIED_REMAINS =
 //            CONFIGURED_FEATURES.register("ore_buried_remains", 
 //                    () -> new ConfiguredFeature<>(SHALLOW_GRAVE_FEATURE.get(), 
 //                            new CountConfiguration(1)));
 //
-//    @SuppressWarnings("deprecation")
-//    public static RegistryObject<ConfiguredFeature<GlowLichenConfiguration,?>> PATCH_ROTTEN_PLANT = 
-//    CONFIGURED_FEATURES.register("patch_rotten_plant",
-//            ()->new ConfiguredFeature<>(ROTTEN_PLANT_FEATURE.get(),
-//                    new GlowLichenConfiguration(20, true, false, false, 1.0F, 
-//                                  HolderSet.direct(Block::builtInRegistryHolder, 
-//                                          List.of(Blocks.STONE, Blocks.DEEPSLATE, Blocks.DIORITE, Blocks.GRANITE, Blocks.ANDESITE,
-//                                                  Blocks.TUFF, Blocks.SMOOTH_BASALT)))));
 
     /** PLACED_FEATURES REGISTRY */
     public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
@@ -91,12 +62,6 @@ public class ModFeatures
 //                     ()->createPlacedVegetationFeature(BLAZE_TREES.getHolder().get(),
 //                             List.of(CountPlacement.of(30), InSquarePlacement.spread(), PlacementUtils.FULL_RANGE)));
 //
-//     public static RegistryObject<PlacedFeature> PATCH_FLAX_COMMON = 
-//             PLACED_FEATURES.register("patch_flax_common", 
-//                     ()->createPlacedPatchFeature(PATCH_FLAX.getHolder().get(), 
-//                             List.of(RarityFilter.onAverageOnceEvery(16), InSquarePlacement.spread(), 
-//                                         PlacementUtils.HEIGHTMAP_WORLD_SURFACE)));
-//                     
 //     public static RegistryObject<PlacedFeature> PATCH_ROTTEN_PLANT_NORMAL = 
 //             PLACED_FEATURES.register("patch_rotten_plant_normal",
 //                     ()->createPlacedCaveFeature(PATCH_ROTTEN_PLANT.getHolder().get(),
@@ -116,11 +81,6 @@ public class ModFeatures
 //                             InSquarePlacement.spread() 
 //                             )));
 //
-//     public static RegistryObject<PlacedFeature> ENDER_CLAMS_OCEAN = 
-//             PLACED_FEATURES.register("ender_clams_ocean", 
-//                     ()->createPlacedCountFeature(ENDER_CLAM.getHolder().get(),
-//                             List.of(RarityFilter.onAverageOnceEvery(8), InSquarePlacement.spread(), 
-//                                     PlacementUtils.HEIGHTMAP_TOP_SOLID)));
      
 
      /** STATIC HELPER METHODS **/
